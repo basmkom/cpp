6cb599ed5805004eba877a4d63065148fcf709af
basmokom <emkua32@gmail.com>
2024-10-27 23:41:21 +0300

    maps

---
+++ maps.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main(){
+    map <string, string> myDictionary;
+    myDictionary.insert(pair <string,string>("ugali", "obokuma"));
+    myDictionary.insert(pair <string,string>("banana", "ritoke"));
+    myDictionary.insert(pair <string,string>("avacado", "avocadoh"));
+    myDictionary.insert(pair <string,string>("skuma", "eskuma"));
+
+for (auto pair : myDictionary){
+    cout <<  pair.first << "   " + pair.second <<endl;
+
+}
+
+    cout<< "my name is elvis";
+}
